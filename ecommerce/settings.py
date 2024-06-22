94797371ac9ee0c6e7f0154bca6f927a9817f58b
Bryan Rafael Andia <bryan@Bryans-Mac-mini.local>
2024-06-21 23:08:40 -0500

    settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -130,12 +130,12 @@ WSGI_APPLICATION = "ecommerce.wsgi.application"
 #         "PORT": 3306,
 #     }
 # }
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 
 # Password validation
